af6163ab509827ac930359db7f738102aa8f3b80
Vladyslav Gudikov <vgoodko@gmail.com>
2017-11-10 08:02:30 +0200

    Hotfix

--- src/lesson-1/tasks/task-1/after/index.js
+++ src/lesson-1/tasks/task-1/after/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export function func(duration, frequency, callback){
+export default function func(duration, frequency, callback){
     for (let i = 0; i <= duration; i++) {
         setTimeout(function () {
             console.log(callback(i));}, frequency * i);
--- src/lesson-1/tasks/task-1/before/index.js
+++ src/lesson-1/tasks/task-1/before/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { func } from '../after';
+import func from '../after';
 
 func(10, 1000, function(item) {
     return item * 2;
