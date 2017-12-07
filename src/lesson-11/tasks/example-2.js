c96704356722195c1a2f52b1ca8c69e48012aa87
Vladyslav Gudikov <vgoodko@gmail.com>
2017-12-07 16:24:32 +0200

    Homework 11

--- src/lesson-11/tasks/example-2.js
+++ src/lesson-11/tasks/example-2.js
@@ -1,12 +1,38 @@
+'use strict';
+
 import fetch from 'isomorphic-fetch';
 
-fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=886faf57284140dd830bf5d6ddadd32f')
+const first = fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=03a3403fe48b417191c714d60baf7fdf&country=us', )
     .then(function(response) {
         if (response.status >= 400) {
             throw new Error("Bad response from server");
         }
         return response.json();
-    })
-    .then(function(stories) {
-        console.log(stories);
     });
+const second = fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=03a3403fe48b417191c714d60baf7fdf&country=uskaka', )
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    });
+const third = fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=03a3403fe48b417191c714d60baf7fdf&country=us', )
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    });
+
+Promise.all([first,second,third]).then(function(stories) {
+    stories.forEach(({articles}) => {
+        articles.forEach(({author, title, description}) => {
+            console.log("Author: ", author);
+            console.log("Title: ", title);
+            console.log("Description: ", description)
+        })
+    });
+}, function (message) {
+    console.log(message)
+
+});
